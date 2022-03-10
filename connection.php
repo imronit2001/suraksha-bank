d3119fd4cc4b24659a7b9d14a165e3f597601fe9
RONIT SINGH <ronitsingh7003@gmail.com>
2022-03-10 19:53:21 +0530

    Database Table modified in db-tables.xsl and Project synopsis finalized in project-details folder

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "suraksha-bank";
+    $conn = mysqli_connect($hostname,$username,$password,$database);
+
+?>
