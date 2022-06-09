b01d6f73e5cc93d04440a29682a38e70eeb89b56
GauravJhamfp <gaurav.jha.1703@gmail.com>
2022-06-09 23:13:46 +0530

    branch table migration Added

---
+++ database/migrations/2022_06_09_082321_create_branches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->id();
+            $table->string('branch_id_prefix')->default('SKB');
+            $table->integer('branch_code')->unique();
+            $table->string('branch_name');
+            $table->string('city');
+            $table->string('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches');
+    }
+};
