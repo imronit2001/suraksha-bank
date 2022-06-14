8b4ed5504f05757db944994f08d2b2b3df065bde
RONIT SINGH <ronitsingh7003@gmail.com>
2022-06-15 01:18:45 +0530

    Helpline Mail Done.. Branch Code generating dynamically Done

--- resources/views/Mailing/debit-credit-mail.blade.php
+++ resources/views/Mailing/debit-credit-mail.blade.php
@@ -3,19 +3,19 @@
 
 @if ( $data["mode"] == "credit")
 
-<div>
+
     <p>Dear <strong>{{ $data["name"] }},</strong></p>
     <p>This is to inform you that - <strong>Rs. {{ $data["amount"] }}</strong> has been credited to your account {{ $data["accountNo"] }} on {{ $data["date"] }} at {{ $data["time"] }} by ref No. {{ $data["transactionId"] }}.</p>
     <p>Available Balance : {{ $data["balance"] }}</p>
-</div>
+
 
 @elseif( $data["mode"] == "debit")
 
-<div>
+
     <p>Dear <strong>{{ $data["name"] }},</strong></p>
     <p>This is to inform you that - <strong>Rs. {{ $data["amount"] }}</strong> has been debited from your account {{ $data["accountNo"] }} on {{ $data["date"] }} at {{ $data["time"] }} by ref No. {{ $data["transactionId"] }}.</p>
     <p>Available Balance : {{ $data["balance"] }}</p>
-</div>
+
 
 @endif
 Thanks,<br>
